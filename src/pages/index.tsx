a3ede3c9b81c26fa54611c678d4d521e08b1b953
Paul Sotelo <francisco.sotelo@meltstudio.co>
2022-07-17 19:29:07 -0600

    feat: first steps SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../../styles/Home.module.css";
 
-import About from '../components/About'
-import Card from '../components/Card'
-import Footer from '../components/Footer'
-import Skills from '../components/Skills'
-import ContactMe from '../components/Contact'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import About from "../components/About";
+import Card from "../components/Card";
+import Footer from "../components/Footer";
+import Skills from "../components/Skills";
+import ContactMe from "../components/Contact";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Home: NextPage = () => {
- useEffect(() => {
+  useEffect(() => {
     AOS.init({
       once: true,
-        
-    })
-  })
+    });
+  });
   return (
     <div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-100">
+      <title>Francisco Sotelo - Portfolio</title>
+      <meta name="description" content="This is my personal portfolio, hello" />
+      <link rel="icon" href="/favicon.ico" />
       <div data-aos="fade-down" data-aos-duration="800">
         <Card />
       </div>
